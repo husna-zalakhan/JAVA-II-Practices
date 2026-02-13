dd49424f129a04e108cf1fb5b7fccebbfb9ccdb6
husna-zalakhan <husna.zalakhan@gmail.com>
2026-02-13 03:54:09 +0000

    JAVA methods

--- App.java
+++ App.java
@@ -8,17 +8,21 @@ public class App{
         System.out.println("Hello, " + name_Std2 +  "!");
     }
    
-//Create a ,ethod  ------> to get average
-public static void getAvrg(int num1, int num2){
+//Create a method ------> to get average
+public static int getAvrg(int num1, int num2){
     int avrg = (num1+ num2)/2;
-    System.out.println("The average is: " + avrg);
-
+    
+    return avrg;
 }
  public static void main(String[] args) {
+    // we do print inside the main function 
+    // instead we are doing the return inside the function itself.   
     sayHello("Husna", "Nazly");
-    getAvrg(10, 20);
+    int average= getAvrg(10, 20);
+    System.out.println("The average is: " + average);
 
 }
+
 
 
 
