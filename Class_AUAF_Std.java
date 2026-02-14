516a8d6c2e712253958e9d1633c397b17ecf7a5c
husna-zalakhan <husna.zalakhan@gmail.com>
2026-02-14 08:30:21 +0000

    Getter and Setter methods

--- Class_AUAF_Std.java
+++ Class_AUAF_Std.java
@@ -4,11 +4,24 @@ public class Class_AUAF_Std {
     //it is called instance field or class field: this belongs to class  
     //instaces variable of the class, it comes with different data structure
     // we use this kind of value when we want to instantiate an object out of this class.    
-    String name;
+    private String name; //instance variable
     int stdId;
     String Concentration;
     String Address;
     int YearOfStudy;
+
+    public void setName(String name){ //argument class
+        this.name= name; //this name belong to argment class. 
+    }   //this.name belong to instance variable
+    public String getName(){
+        return name;
+
+    }
+
+
+
+
+
 
     public void display() { 
         System.out.println("Name: " + name);
