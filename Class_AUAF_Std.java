c867063940df125743a7aa8ccde6f176a629a4b4
husna-zalakhan <husna.zalakhan@gmail.com>
2026-02-14 06:42:53 +0000

    classes and objects

--- Class_AUAF_Std.java
+++ Class_AUAF_Std.java
@@ -1,11 +1,24 @@
 public class Class_AUAF_Std {
     
+    
     //it is called instance field or class field: this belongs to class  
+    //instaces variable of the class, it comes with different data structure
     // we use this kind of value when we want to instantiate an object out of this class.    
     String name;
-    int StdID;
+    int stdId;
     String Concentration;
-    String ddress;
+    String Address;
     int YearOfStudy;
+
+    public void display() { 
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + stdId );
+        System.out.println("Concentration: " + Concentration );
+        System.out.println("Address: " + Address);
+        System.out.println("Year of Study: " + YearOfStudy);
+
+
+    }
+    
 
 }
