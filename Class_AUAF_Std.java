93e57dd77f6bf2be9acf2fefa615350b642326b8
husna-zalakhan <husna.zalakhan@gmail.com>
2026-02-14 05:12:56 +0000

    java: instantiate obj out of class

---
+++ Class_AUAF_Std.java
@@ -0,0 +1,11 @@
+public class Class_AUAF_Std {
+    
+    //it is called instance field or class field: this belongs to class  
+    // we use this kind of value when we want to instantiate an object out of this class.    
+    String name;
+    int StdID;
+    String Concentration;
+    String ddress;
+    int YearOfStudy;
+
+}
